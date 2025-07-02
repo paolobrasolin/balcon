12095d768a4162a3947c0caf4f8c0af3fc14b8cd
Paolo Brasolin <paolo.brasolin@gmail.com>
2025-07-02 14:07:38 +0200

    Setup nix dev env

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs.nixpkgs.url = "github:Nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            bun
+          ];
+        };
+      }
+    );
+}
